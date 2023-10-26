a52d80bd5684a52bc72cdb1761e8313ce9704fb6
CamilleLAR <camille.l.a.richet@gmail.com>
2023-10-26 17:33:46 +0100

    app page html structure added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,9 @@
-import { useState } from 'react'
+import { useState } from "react";
 //import reactLogo from './assets/react.svg'
 //import viteLogo from '/vite.svg'
-import './App.css'
-import ExistingUserView from './components/ExistingUserView';
-import NewUserView from './components/NewUserView';
+import "./App.css";
+import ExistingUserView from "./components/ExistingUserView";
+import NewUserView from "./components/NewUserView";
 
 function App() {
   const [isNew, setIsNew] = useState(true);
@@ -13,20 +13,30 @@ function App() {
   };
 
   return (
-      <div className="appContainer">
-        <aside className='viewButtons'>
-          <button className="oldUserButton" style={{backgroundColor: isNew? "#8DB580":""}} onClick={() => handleChangeView(true)}>Registered User</button>        
-          <button className="newUserButton" style={{backgroundColor: isNew? "":"#8DB580"}} onClick={() => handleChangeView(false)}>New User</button>
+    <div className="appContainer">
+      <header>
+        <aside className="viewButtons">
+          <button
+            className="oldUserButton"
+            style={{ backgroundColor: isNew ? "#8DB580" : "" }}
+            onClick={() => handleChangeView(true)}
+          >
+            Registered User
+          </button>
+          <button
+            className="newUserButton"
+            style={{ backgroundColor: isNew ? "" : "#8DB580" }}
+            onClick={() => handleChangeView(false)}
+          >
+            New User
+          </button>
         </aside>
-        <h1>Ingredients Finder</h1>
-        {isNew? (
-        <ExistingUserView />
-        ) : (
-        <NewUserView />
-        )}
 
-      </div>
+        <h1>Ingredients Finder</h1>
+      </header>
+      <body>{isNew ? <ExistingUserView /> : <NewUserView />}</body>
+    </div>
   );
 }
 
-export default App
+export default App;
