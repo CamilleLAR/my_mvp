7f80839af020febc078ec2f24378081e59104bb3
CamilleLAR <camille.l.a.richet@gmail.com>
2023-10-26 16:45:12 +0100

    login page styling update

--- client/src/components/ExistingUserView.jsx
+++ client/src/components/ExistingUserView.jsx
@@ -4,10 +4,11 @@ function AdminView() {
   
 
   return (
-    <div>
+    <div className="existingUserLoginPage">
         <h2>Welcome back!</h2>
         <form className="loginForm">
             <input className="userID" type="text" placeholder="Username" />
+            <br/>
             <input className="userPW" type="text" placeholder="Password"/>
             <br/>
             <button className="loginBtn">Login</button>
